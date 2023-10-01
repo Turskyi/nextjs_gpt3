d00404d479b12d9c3cf526858278e722ce92e0fd
Dmytro Turskyi <dmytro.turskyi@gmail.com>
2023-09-30 21:08:48 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# Politer Ai Web site
+
+This is a [Next.js](https://nextjs.org/) project bootstrapped
+with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+
+It follows
+the [Build and Deploy a GPT-3 App in NextJS in 1 Hour (ChatGPT, TypeScript)](https://youtu.be/5i1Q2GSqidU?si=yJQPo4ToK31t9wEy)
+by Coding in Flow YouTube channel.
+
+## Features
+* openai
+* TypeScript
+* ESLint
+* CSS modules
+* Styling
+
+## Getting Started
+
+To run this project locally, you must have Node.js on your machine.
+First, run the development server:
+
+```bash
+npm run dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+
+You can start editing the page by modifying `pages/index.tsx`. The page auto-updates as you edit the file.
+
+[API routes](https://nextjs.org/docs/api-routes/introduction) can be accessed
+on [http://localhost:3000/api/polite](http://localhost:3000/api/polite). This endpoint can be edited
+in `pages/api/polite.ts`.
+
+The `pages/api` directory is mapped to `/api/*`. Files in this directory are treated
+as [API routes](https://nextjs.org/docs/api-routes/introduction) instead of React pages.
+
+This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and
+load Inter, a custom Google Font.
+
+## Credits
+
+This project is based on
+the [Build and Deploy a GPT-3 App in NextJS in 1 Hour (ChatGPT, TypeScript)](https://youtu.be/5i1Q2GSqidU?si=yJQPo4ToK31t9wEy)
+by [Coding in Flow](https://github.com/codinginflow) YouTube channel.
+All credit goes to the original author [Florian Walther](https://github.com/florianwalther-private). I only followed
+along and made some minor changes.
---
+++ pages/index.tsx
@@ -0,0 +1,82 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+import mainImage from '@/assets/images/main_horizontal_image.png'
+import { Button, Form, Spinner } from 'react-bootstrap'
+import { FormEvent, useState } from 'react'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+
+  const [message, setMessage] = useState('');
+  const [messageLoading, setMessageLoading] = useState(false);
+  const [messageLoadingError, setMessageLoadingError] = useState(false);
+
+  async function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const formData = new FormData(event.target as HTMLFormElement);
+    const prompt = formData.get("prompt")?.toString().trim();
+
+    if (prompt) {
+      try {
+        setMessage('');
+        setMessageLoadingError(false);
+        setMessageLoading(true);
+
+        const response = await fetch("/api/polite?prompt=" + encodeURIComponent(prompt));
+        const body = await response.json();
+        setMessage(body.quote);
+      } catch (error) {
+        console.error(error);
+        setMessageLoadingError(true);
+      } finally {
+        setMessageLoading(false);
+      }
+    }
+  }
+
+  return (
+    <>
+      <Head>
+        <title>Politer AI: The ultimate tool to make your messages more polite and friendly</title>
+        <meta name="description" content="by Dmytro Turskyi" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={`${styles.main} ${inter.className}`}>
+        <h1>Politer AI</h1>
+        <h2>powered by GPT-3</h2>
+        <div>Type your message that you want to make more polite and friendly.</div>
+        <div>Politer AI will transform your message into a more courteous and respectful one.</div>
+        <div>So you can maintain a positive relationship with your recipient.</div>
+        <div className={styles.mainImageContainer}>
+          <Image
+            src={mainImage}
+            fill
+            alt='A picture of a text transform your message and the image of two people talking to each other'
+            priority
+            className={styles.mainImage}
+          />
+        </div>
+        <Form onSubmit={handleSubmit} className={styles.inputForm}>
+          <Form.Group className='mb-3' controlId='prompt-imput'>
+            <Form.Label>Type your message below and click the button to make it more polite and friendly.</Form.Label>
+            <Form.Control
+              name='prompt'
+              placeholder='e.g. I don not have time for this nonsense. Stop bothering me.'
+              maxLength={100}
+            />
+          </Form.Group>
+          <Button type='submit' className='mb-3' disabled={messageLoading}>
+            Polite it up 😊
+          </Button>
+        </Form>
+        {messageLoading && <Spinner animation='border' />}
+        {messageLoadingError && 'Something went wrong. Please try again.'}
+        {message && <h5>{message}</h5>}
+      </main>
+    </>
+  )
+}
