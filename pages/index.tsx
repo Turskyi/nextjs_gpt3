51605937e781d3718223b55ad0fe0d711271ddc2
Dmytro Turskyi <dmytro.turskyi@gmail.com>
2023-10-01 22:01:16 -0400

    make title shorter

--- pages/index.tsx
+++ pages/index.tsx
@@ -43,7 +43,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Politer AI: The ultimate tool to make your messages more polite and friendly</title>
+        <title>Politer AI: Make your messages nicer</title>
         <meta name="description" content="by Dmytro Turskyi" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
