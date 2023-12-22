096ea869659c5592bcdc0fce2ac562a34f783330
Dmytro Turskyi <dmytro.turskyi@gmail.com>
2023-12-22 18:15:33 -0500

    Standardize syntax and reinforce code consistency
    Cleaned up inconsistencies across various files by enforcing semicolons,
    proper quotes, and standard line endings. Adjusted font declarations in
    CSS to use multi-line values for readability. Enhanced web manifest with
    spacing for clarity and ensured no files are lacking a newline at EOF as
    per standard practice. These changes do not impact functionality but
    improve code quality and maintenance ease.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,28 +1,28 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  plugins: [require("daisyui")],
+  plugins: [require('daisyui')],
   daisyui: {
     themes: [
       {
         lightTheme: {
-          primary: "#dfcaff",
-          secondary: "#00b200",
-          accent: "#6f3f00",
-          neutral: "#24151e",
-          "base-100": "#f5fdff",
-          info: "#008da6",
-          success: "#00aa12",
-          warning: "#985a00",
-          error: "#ff0028",
+          primary: '#dfcaff',
+          secondary: '#00b200',
+          accent: '#6f3f00',
+          neutral: '#24151e',
+          'base-100': '#f5fdff',
+          info: '#008da6',
+          success: '#00aa12',
+          warning: '#985a00',
+          error: '#ff0028',
           body: {
             // This way we can add a dark theme with a diff
-            "background-color": "#dfcaff",
+            'background-color': '#dfcaff',
           },
         },
       },
