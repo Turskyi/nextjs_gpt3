7836ef5d4ac9789cbf241377786c5381432aea07
Dmytro Turskyi <dmytro.turskyi@gmail.com>
2025-03-30 19:35:33 -0400

    :art: Enhance homepage layout with download and screenshot sections; improve styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,21 +2,20 @@ import Head from 'next/head';
 import Image from 'next/image';
 import styles from '@/styles/Home.module.css';
 import mainImage from '@/assets/images/main_horizontal_image.png';
+import appStoreBadgePath from '@/assets/images/Download_on_the_App_Store_Badge.png';
+import screenshotTablet1Path from '@/assets/images/screenshot1.png';
+import screenshotPhone1Path from '@/assets/images/screenshot2.png';
 import { Button, Form, Spinner } from 'react-bootstrap';
 import { FormEvent, useState } from 'react';
 import { INPUT_MAX_LENGTH } from '../constants';
 import Link from 'next/link';
-import appStoreBadgePath from '@/assets/images/Download_on_the_App_Store_Badge.png';
 
 export default function Home() {
-  // State to track the message input.
   const [message, setMessage] = useState('');
   const [messageLoading, setMessageLoading] = useState(false);
   const [messageLoadingError, setMessageLoadingError] = useState(false);
-  // State to track character count.
   const [charCount, setCharCount] = useState(0);
 
-  // Function to handle form submission.
   async function handleSubmit(event: FormEvent<HTMLFormElement>) {
     setMessage('');
     event.preventDefault();
@@ -45,7 +44,6 @@ export default function Home() {
     }
   }
 
-  // Function to handle input change and update character count
   function handleInputChange(event: React.ChangeEvent<HTMLTextAreaElement>) {
     const value = event.target.value;
     setCharCount(value.length);
@@ -53,24 +51,27 @@ export default function Home() {
 
   return (
     <>
-      {/* Use the Head component to customize the title and meta description 
-      for each page */}
       <Head>
         <title>Politer AI: Make your messages nicer</title>
         <meta
           name="description"
           content="Politer AI is a GPT-3.5 powered tool created by Software 
           Engineer Dmytro Turskyi, that helps you write more polite and 
-          friendly messages. Just type your message and click the button to see 
-          the magic."
+          friendly messages. Just type your message and click the button to 
+          see the magic."
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main}`}>
+        {/* Header Section */}
         <h1 className={styles.heading}>Politer AI</h1>
-        <h2 className={styles.subtitle}>powered by GPT-3.5</h2>
-        <br></br>
+        <p className={styles.tagline}>
+          Transform your messages into polite and professional communication
+          with the power of AI.
+        </p>
+
+        {/* Input Form Section */}
         <Form onSubmit={handleSubmit} className={styles.inputForm}>
           <Form.Group className="mb-3" controlId="prompt-input">
             <Form.Label>
@@ -79,12 +80,10 @@ export default function Home() {
             </Form.Label>
             <Form.Control
               name="prompt"
-              placeholder="e.g. I do not have time for this nonsense. Stop 
-              bothering me."
+              placeholder="e.g. This makes no sense at all."
               maxLength={INPUT_MAX_LENGTH}
               as="textarea"
               rows={3}
-              // Call handleInputChange on input change.
               onChange={handleInputChange}
             />
             <p>
@@ -96,12 +95,14 @@ export default function Home() {
           </Button>
         </Form>
         {messageLoading && <Spinner animation="border" />}
-        {messageLoadingError &&
-          `We apologize for the inconvenience, but the 
-          OpenAI API is not available at the moment. It looks like we have 
-          reached our limit or quota for the API. Please wait for a while or 
-          switch to a different service.`}
-
+        {messageLoadingError && (
+          <p className={styles.error}>
+            We apologize for the inconvenience, but the OpenAI API is not
+            available at the moment. It looks like we have reached our limit or
+            quota for the API. Please wait for a while or switch to a different
+            service.
+          </p>
+        )}
         {message && (
           <div className={styles.politeContainer}>
             <h5 className={styles.politeTitle}>Polite version:</h5>
@@ -111,7 +112,8 @@ export default function Home() {
           </div>
         )}
 
-        {message.length == 0 && (
+        {/* Main Image Section */}
+        {message.length === 0 && (
           <div className={styles.mainImageContainer}>
             <Image
               src={mainImage}
@@ -122,35 +124,22 @@ export default function Home() {
             />
           </div>
         )}
+
+        {/* Features Section */}
         <section className={styles.featuresSection}>
           <h2>Why Use Politer AI?</h2>
           <ul className={styles.featuresList}>
             <li>Instantly make your messages more polite and professional</li>
-            <li>Easy-to-use interface</li>
-            <li>Powered by AI to enhance your communication</li>
-            <li>Available for free on Android</li>
+            <li>Easy-to-use interface powered by GPT-3.5</li>
+            <li>Enhance your communication with AI</li>
+            <li>Available for free on Android and iOS</li>
           </ul>
         </section>
-        <div
-          style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            flexDirection: 'column',
-            paddingBottom: '20px',
-            width: '100%',
-          }}
-        >
+
+        {/* Download Section */}
+        <div className={styles.downloadSection}>
           <h3>Download Politer AI</h3>
-          <div
-            style={{
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-              gap: '10px', // Add spacing between the badges
-              marginBottom: '10px',
-            }}
-          >
+          <div className={styles.downloadBadges}>
             <a
               href="https://play.google.com/store/apps/details?id=com.turskyi.politerai"
               target="_blank"
@@ -177,6 +166,21 @@ export default function Home() {
             </a>
           </div>
         </div>
+
+        {/* Screenshots Section */}
+        <section className={styles.screenshotSection}>
+          <h2>App Screenshots</h2>
+          <div className={styles.screenshots}>
+            <Image
+              src={screenshotTablet1Path}
+              alt="Screenshot 1"
+              height={600}
+            />
+            <Image src={screenshotPhone1Path} alt="Screenshot 2" height={600} />
+          </div>
+        </section>
+
+        {/* Support Link */}
         <Link
           href="/support"
           style={{
